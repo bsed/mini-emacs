ba74fcb6f33754f4353556ccd7f038abd9306662
bsed <me@gnux.cn>
2021-01-17 16:59:48 +0800

    first commit

---
+++ elpa/go-guru-20181012.330/go-guru-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/kelvin/.emacs.d/elpa/go-guru-20181012.330/go-guru.el  -*- no-byte-compile: t -*-
+(define-package "go-guru" "20181012.330" "Integration of the Go 'guru' analysis tool into Emacs." '((go-mode "1.3.1") (cl-lib "0.5")) :commit "fdf46fe0e110a8e0dddb5aac4ab20a93ee9c5d88" :keywords '("tools"))
